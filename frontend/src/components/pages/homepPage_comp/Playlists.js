8c87c5de41414c2d406aa19d0329016ca3807fed
benconte <benconte201@gmail.com>
2021-10-30 11:05:20 +0000

    added css files

--- frontend/src/components/pages/homepPage_comp/Playlists.js
+++ frontend/src/components/pages/homepPage_comp/Playlists.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import Slider from "react-slick"
 
 function Playlists() {
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: false
+    }
     return (
         <div>
             <h2>These are the Playlists</h2>
